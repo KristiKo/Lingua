b03bbdbc9c8febc7609b6e48a7487b50c9555c43
Matteo <matteo.fusilli90@gmail.com>
2019-02-23 22:14:12 +0000

    updated dashboard

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
--- lingua/static/js/script.js
+++ lingua/static/js/script.js
@@ -26,11 +26,10 @@ function toggleAccordion(){
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
+/* Dashboard */
 $( document ).ready(function() {
 
-  /* Dashboard */
   $(".main-sub-menu").on("click",function(){
-    $(".sub-menu").css({"display":"none"});
     $('.main-sub-menu').find(".fa-angle-right").css({"transform":"rotate(0deg)"});
     $(this).find("ul").slideToggle();
     $(".main-menu .main-sub-menu").removeClass("dashboard-active");
