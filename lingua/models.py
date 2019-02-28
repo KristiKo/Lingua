4520c792091ae60cfb07f3971adaa43cb666e455
Matteo <matteo.fusilli90@gmail.com>
2019-02-28 22:35:09 +0000

    added extra fields on courses

--- lingua/models.py
+++ lingua/models.py
@@ -23,7 +23,8 @@ class Programme(models.Model):
     description = models.TextField(null=True)
     attendance = models.CharField(max_length=140, null=True)
     learning_mode = models.CharField(max_length=140, null=True)
-    cost = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    original_cost = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    discount_cost = models.DecimalField(max_digits=7, decimal_places=2, null=True)
 
     def __str__(self):
         return self.name
@@ -51,7 +52,30 @@ class Course(models.Model):
     description = models.TextField(null=True)
     attendance = models.CharField(max_length=140, null=True)
     learning_mode = models.CharField(max_length=140, null=True)
-    cost = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    student_per_class = models.IntegerField(null=True)
+    lesson_durations = models.IntegerField(null=True)
+    option1 = models.CharField(max_length=140, null=True, blank=True)
+    original_cost1 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    discount_cost1 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    option2 = models.CharField(max_length=140, null=True, blank=True)
+    original_cost2 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    discount_cost2 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    option3 = models.CharField(max_length=140, null=True, blank=True)
+    original_cost3 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    discount_cost3 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    option4 = models.CharField(max_length=140, null=True, blank=True)
+    original_cost4 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    discount_cost4 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    option5 = models.CharField(max_length=140, null=True, blank=True)
+    original_cost5 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    discount_cost5 = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    housing = models.BooleanField(null=True)
+    housing_description = models.TextField(null=True, blank=True)
+    min_age = models.IntegerField(null=True)
+    distance = models.CharField(max_length=140, null=True)
+    meals = models.CharField(max_length=140, null=True)
+    housing_cost = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+
 
     def __str__(self):
         return self.name
