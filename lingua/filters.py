fc095479825f96fe6f3230f8828c72d3f1128021
Matteo <matteo.fusilli90@gmail.com>
2019-03-21 18:05:14 +0000

    improved filters

--- lingua/filters.py
+++ lingua/filters.py
@@ -4,21 +4,26 @@ import django_filters
 
 class ProgrammeFilter(django_filters.FilterSet):
     university = django_filters.ModelChoiceFilter(queryset=University.objects.all())
-    name = django_filters.CharFilter(lookup_expr='icontains')
-    cost = django_filters.NumberFilter(field_name='original_cost', lookup_expr='lt') # 'gt' for min range value 'lt' for max range value
-    
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    city = django_filters.ModelChoiceFilter(queryset=University.objects.all().values_list('city', flat=True))
+    award = django_filters.CharFilter(field_name='award', lookup_expr='icontains')
+    max_cost = django_filters.NumberFilter(field_name='original_cost', lookup_expr='lt')
+    min_cost = django_filters.NumberFilter(field_name='original_cost', lookup_expr='gt')
 
     class Meta:
         model = Programme
-        fields = ['university', 'name', 'cost']
+        fields = ['university', 'name', 'city', 'award', 'max_cost', 'min_cost']
 
 
 class CourseFilter(django_filters.FilterSet):
     college = django_filters.ModelChoiceFilter(queryset=College.objects.all())
-    name = django_filters.CharFilter(lookup_expr='icontains')
-    cost = django_filters.NumberFilter(field_name='original_cost1', lookup_expr='lt')
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    city = django_filters.ModelChoiceFilter(queryset=College.objects.all().values_list('city', flat=True))
+    max_cost = django_filters.NumberFilter(field_name='original_cost1', lookup_expr='lt')
+    min_cost = django_filters.NumberFilter(field_name='original_cost1', lookup_expr='gt')
+    housing = django_filters.BooleanFilter()
     
 
     class Meta:
         model = Course
-        fields = ['college', 'name', 'cost']
+        fields = ['college', 'name', 'city', 'max_cost', 'min_cost', 'housing']
